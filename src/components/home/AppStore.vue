ffad34c9f956e833044cc5c285990efc7a77fabe
alessiafabbioni <alessiafabbioni@gmail.com>
2024-01-16 15:44:11 +0100

    App store update

--- src/components/home/AppStore.vue
+++ src/components/home/AppStore.vue
@@ -7,38 +7,34 @@ export default {
 
 <template>
     <section class="container">
-        <div class="row">
-            <div class="col-lg-3 col-md-3 col-sm-6">
-                <div class="icon-card">
-                    <div class="icon-container icon1"></div>
-                    <span class="icon-title">Free Delivery</span>
-                    <span class="icon-info">Free Shipping on all orders</span>
+        <div class="row mt-7">
+            <div class="col-lg-6 col-md-12 col-sm-12 mb-5 column-gap-5">
+                <div class="card-promo">
+                    <img src="https://capricathemes.com/wordpress/WCM04/WCM040086/wp-content/uploads/2020/08/cms-banner-01.jpg" alt="most popular">
+                    <div class="card-promo-info">
+                        <h3>Most Popular</h3>
+                        <h3>The Battlefield 4 Naval Strike</h3>
+
+                    </div>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-3 col-sm-6">
-                <div class="icon-card">
-                    <div class="icon-container icon2"></div>
-                    <span class="icon-title">Money Return</span>
-                    <span class="icon-info">Back Guarantee in 7 days</span>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-6">
-                <div class="icon-card">
-                    <div class="icon-container icon3"></div>
-                    <span class="icon-title">Member Discount</span>
-                    <span class="icon-info">On every order over $130.00</span>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-6">
-                <div class="icon-card">
-                    <div class="icon-container icon4"></div>
-                    <span class="icon-title">Return Policy</span>
-                    <span class="icon-info">Support 24 hours a day</span>
+            <div class="col-lg-6 col-md-12 col-sm-12 mb-5 column-gap-5">
+                <div class="card-promo">
+                    <img src="https://capricathemes.com/wordpress/WCM04/WCM040086/wp-content/uploads/2020/08/cms-banner-02.jpg" alt="most popular">
+                    <div class="card-promo-info">
+                        <h3>Most Popular</h3>
+                        <h3>The Battlefield 4 Naval Strike</h3>
+                    </div>
                 </div>
             </div>
         </div>
+        <div class="mt-7">
+
+        </div>
 
     </section>
+
+
 </template>
 
 <style lang="scss" scoped>
@@ -46,50 +42,32 @@ export default {
 @use 'src/styles/partials/_mixins.scss' as *;
 
 
-.icon-card {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin: 20px 0;
-    padding: 30px;
-}
-.icon-container {
-            width: 60px;
-            height: 60px;
-            background-size: 100% auto;
-            background-position: center;
-            margin-bottom: 10px; 
-        }
+.card-promo {
+    object-fit: cover;
+    position: relative;
+    overflow: hidden;
 
-.icon1 { 
-    background-image: url('public/img/service-icon.png'); 
-    background-position: 0% 12%; 
+
 }
 
-.icon2 { 
-    background-image: url('public/img/service-icon.png'); 
-    background-position: 0% 42%; 
-}
+.card-promo-info {
+    position: absolute;
+    top: 10%;
+    right: 10%;
+    width: 40%;
 
-.icon3 { 
-    background-image: url('public/img/service-icon.png'); 
-    background-position: 0% 71%; 
-}
+    h3:first-child{
+        background-color: #f8a900;
+        color: black;
+        font-weight: 600;
+    }
 
-.icon4 { 
-    background-image: url('public/img/service-icon.png'); 
-    background-position: 0% 100%; 
-}
+    h3 {
+        color: white;
+        font-weight: 600;
+    }
 
-.icon-title {
-    font-weight: 700;
-    color: white;
-}
-
-.icon-info{
-    color: #A6A6A6;
-}
+    }
 
 
 
