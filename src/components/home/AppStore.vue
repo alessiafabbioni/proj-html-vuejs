895be9a48482e2d16686dbf578b6364c6bda2493
alessiafabbioni <alessiafabbioni@gmail.com>
2024-01-16 12:38:13 +0100

    ADD: icons home

--- src/components/home/AppStore.vue
+++ src/components/home/AppStore.vue
@@ -11,29 +11,29 @@ export default {
             <div class="col">
                 <div class="icon-card">
                     <div class="icon-container icon1"></div>
-                    <span>Free Delivery</span>
-                    <span>Free Shipping on all orders</span>
+                    <span class="icon-title">Free Delivery</span>
+                    <span class="icon-info">Free Shipping on all orders</span>
                 </div>
             </div>
             <div class="col">
                 <div class="icon-card">
-                    <div class="icon-container icon1"></div>
-                    <span>Free Delivery</span>
-                    <span>Free Shipping on all orders</span>
+                    <div class="icon-container icon2"></div>
+                    <span class="icon-title">Money Return</span>
+                    <span class="icon-info">Back Guarantee in 7 days</span>
                 </div>
             </div>
             <div class="col">
                 <div class="icon-card">
-                    <div class="icon-container icon1"></div>
-                    <span>Free Delivery</span>
-                    <span>Free Shipping on all orders</span>
+                    <div class="icon-container icon3"></div>
+                    <span class="icon-title">Member Discount</span>
+                    <span class="icon-info">On every order over $130.00</span>
                 </div>
             </div>
             <div class="col">
                 <div class="icon-card">
-                    <div class="icon-container icon1"></div>
-                    <span>Free Delivery</span>
-                    <span>Free Shipping on all orders</span>
+                    <div class="icon-container icon4"></div>
+                    <span class="icon-title">Return Policy</span>
+                    <span class="icon-info">Support 24 hours a day</span>
                 </div>
             </div>
             
@@ -47,16 +47,13 @@ export default {
 @use 'src/styles/partials/_variables.scss' as *;
 @use 'src/styles/partials/_mixins.scss' as *;
 
-* {
-    color: white;
-}
 
 .icon-card {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    margin: 20px;
+    margin: 20px 0;
     padding: 30px;
 }
 .icon-container {
@@ -67,10 +64,34 @@ export default {
             margin-bottom: 10px; 
         }
 
-        .icon1 { 
-            background-image: url('public/img/service-icon.png'); 
-            background-position: 0% 13%; 
-        }
-        .icon2 { background-image: url('logos.jpg'); background-position: 0% 2%; }
+.icon1 { 
+    background-image: url('public/img/service-icon.png'); 
+    background-position: 0% 12%; 
+}
+
+.icon2 { 
+    background-image: url('public/img/service-icon.png'); 
+    background-position: 0% 42%; 
+}
+
+.icon3 { 
+    background-image: url('public/img/service-icon.png'); 
+    background-position: 0% 71%; 
+}
+
+.icon4 { 
+    background-image: url('public/img/service-icon.png'); 
+    background-position: 0% 100%; 
+}
+
+.icon-title {
+    font-weight: 700;
+    color: white;
+}
+
+.icon-info{
+    color: #A6A6A6;
+}
+
 
 </style>
