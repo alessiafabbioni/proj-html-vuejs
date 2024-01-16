f6c0159983e01424160988896875f529ff39121b
Gabriele Di Rocco <gabriele.dirocco000@gmail.com>
2024-01-16 17:34:59 +0100

    why choose us section - layout di base

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -7,7 +7,7 @@ export default {
 <template>
   <section class="container-fluid pt-2" id="About-us">
     <div class="container">
-      <div class="row">
+      <div class="row bread-crumb">
         <div class="col-lg-6">
           <nav>
             <ol class="breadcrumb">
@@ -16,12 +16,82 @@ export default {
                   Home
                 </router-link>
               </li>
-              <li class="breadcrumb-item" aria-current="page">Library</li>
+              <li class="breadcrumb-item" aria-current="page">About Us</li>
             </ol>
           </nav>
         </div>
         <div class="col-lg-6 d-flex justify-content-end">
-          <h4>About Us</h4>
+          <span>About Us</span>
+        </div>
+      </div>
+      <div class="row">
+        <div
+          id="carouselExampleFade"
+          class="carousel slide carousel-fade col-lg-6"
+          data-bs-ride="carousel"
+        >
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img
+                src="/img/Et-quasi-architecto-beate.jpg"
+                class="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div class="carousel-item">
+              <img
+                src="/img/Laborum-et-dolor-fuga-Et.jpg"
+                class="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div class="carousel-item">
+              <img
+                src="/img/Blandit-praesen-volupta-1400x878_t.jpg"
+                class="d-block w-100"
+                alt="..."
+              />
+            </div>
+          </div>
+          <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleFade"
+            data-bs-slide="prev"
+          >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleFade"
+            data-bs-slide="next"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+        <div class="col-lg-6">
+          <div>
+            <h5>Why choose us</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+              velit! Dolorem, minima sapiente. Alias omnis in nesciunt,
+              molestias optio voluptatibus aliquam labore, error officiis,
+              corrupti commodi eaque a nemo ex? Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Corrupti qui quo nemo. Voluptatibus,
+              aliquam similique cumque alias at, doloremque maiores ipsum
+              obcaecati quos sit, eligendi animi magnam nisi rerum magni!
+            </p>
+            <ul>
+              <li>Praesent sed ex vel mauris ele.</li>
+              <li>Nam vel luctus nulla, eget interdum metus</li>
+              <li>Temporibus autem</li>
+              <li>Excepteur sint occaecat</li>
+            </ul>
+            <div class="lets-talk col-lg-3">Let's TALK</div>
+          </div>
         </div>
       </div>
     </div>
@@ -35,5 +105,14 @@ export default {
   background-image: url('public/img/body-bg.jpg');
   height: 800px;
   color: white;
+  .lets-talk {
+    background-color: #23172f;
+    text-align: center;
+    padding: 10px;
+  }
+  .lets-talk:hover {
+    cursor: pointer;
+    color: #f9aa01;
+  }
 }
 </style>
