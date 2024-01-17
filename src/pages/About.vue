c8b7d84ff65004d94b0329c89c7a6a49cbb52dbc
Gabriele Di Rocco <gabriele.dirocco000@gmail.com>
2024-01-17 17:43:14 +0100

    sezione history in about hover+animazioni

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -133,17 +133,38 @@ export default {
     </div>
 
     <!-- SEZIONE HISTORY -->
-    <div class="history text-center mt-5">
-      <h2>Codezeel History</h2>
-      <div class="row">
+    <div class="history container text-center mt-5">
+      <h2 class="pb-4">Codezeel History</h2>
+      <div class="row mt-5">
         <div class="col-lg-4" v-for="(card, i) in cards" :key="i">
-          <div class="cardz">
+          <div class="cards d-flex flex-column align-items-center gap-3">
             <div class="icon">
               <i :class="card.icon"></i>
             </div>
             <span>{{ card.title }}</span>
             <p>{{ card.text }}</p>
           </div>
+        </div>
+      </div>
+    </div>
+    <!-- banner -->
+    <div class="container counter my-5">
+      <div class="row">
+        <div class="col-lg-3">
+          <h2 class="num">50</h2>
+          <span class="txt">Hours Per Week</span>
+        </div>
+        <div class="col-lg-3">
+          <h2 class="num">25,025</h2>
+          <span class="txt">Line of code</span>
+        </div>
+        <div class="col-lg-3">
+          <h2 class="num">980</h2>
+          <span class="txt">Projects Completed</span>
+        </div>
+        <div class="col-lg-3 border-end-0">
+          <h2 class="num">334</h2>
+          <span class="txt">Shots On Dribble</span>
         </div>
       </div>
     </div>
@@ -191,7 +212,7 @@ export default {
     div {
       color: #a59d95;
       font-size: 15px;
-      font-weight: bold;
+      font-weight: 400;
       gap: 15px;
       .list-element {
         i {
@@ -213,6 +234,58 @@ export default {
     .lets-talk:hover {
       cursor: pointer;
       color: #f9aa01;
+    }
+  }
+  .history {
+    h2 {
+      position: relative;
+    }
+    h2::after {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 50%;
+      transform: translate(-50%);
+      width: 5%;
+      height: 2px;
+      background-color: #f9aa01;
+    }
+    .cards {
+      i {
+        font-size: 30px;
+        width: 60px;
+        height: 60px;
+        color: #000000;
+        background-color: #f9aa01;
+        border-radius: 50%;
+        line-height: 60px;
+        display: inline-block;
+        margin-bottom: 10px;
+        transition: transform 0.2s;
+      }
+      span {
+        font-weight: bold;
+      }
+      p {
+        color: #747276;
+      }
+    }
+    .cards:hover i {
+      transform: scale(1.2);
+    }
+  }
+  .counter {
+    background-color: #170f1f;
+    .row {
+      padding: 50px;
+      div {
+        border-right: solid 1px #241b2e;
+        text-align: center;
+      }
+
+      span {
+        color: #777777;
+      }
     }
   }
   .item {
