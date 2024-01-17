b7ec131dd0164b1982906139e2300c95dacfec59
alessiafabbioni <alessiafabbioni@gmail.com>
2024-01-17 15:05:44 +0100

    Fix link

--- src/components/home/Blog.vue
+++ src/components/home/Blog.vue
@@ -14,7 +14,7 @@ export default {
         <div class="row pb-5">
             <div class="col-lg-4 col-md-12 col-sm-12 column-gap-5 special-car">
                 <div class="card-promo">
-                    <img src="/Users/alessiafabbioni/Desktop/boolean/proj-html-vuejs/public/img/Nisi-ut-aliquid-ex-ea-com.jpg" alt="Batman">
+                    <img src="//capricathemes.com/wordpress/WCM04/WCM040086/wp-content/uploads/2020/08/Nisi-ut-aliquid-ex-ea-com-1400x878_t.jpg" alt="Batman">
                     <div class="row">
                         <div class="col-4  mt-3">
                             <i class="fa fa-calendar pe-2"></i>
