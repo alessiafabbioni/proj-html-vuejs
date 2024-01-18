78a7952644d303e685c7c3caa59560055e73d47e
Gabriele Di Rocco <gabriele.dirocco000@gmail.com>
2024-01-18 11:17:11 +0100

    methods in About

---
+++ src/components/about/Team.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: 'Team'
+}
+</script>
+
+<template>
+  <div class="team text-center">
+    <h3>Our Team Member</h3>
+    <p>Meet the many faces that help power the Elegant Themes community</p>
+  </div>
+  <div class="row">
+    <div class="col"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use 'src/styles/partials/_variables.scss' as *;
+@use 'src/styles/partials/_mixins.scss' as *;
+</style>
